a1b6c352c17c61a565ffa9689773d99c51269380
Pablo Garcia <pgarciag566@gmail.com>
2022-09-24 16:57:27 -0500

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-red-400'>
+        Prueba
+      </h1>
+    </div>
+  );
+}
+
+export default App;
